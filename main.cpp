87fd9ae9c4f8ae79c2853d6e3199142b15bb9ecf
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2024-06-21 12:12:29 +0300

    Create init funciton

--- main.cpp
+++ main.cpp
@@ -51,7 +51,6 @@ SDL_Renderer* gRenderer = NULL;
 
 //Textures
 LTexture gRockTexture;
-LTexture gBackgroundTexture;
 
 LTexture::LTexture()
 {
@@ -134,6 +133,59 @@ int LTexture::getWidth()
 int LTexture::getHeight()
 {
     return mHeight;
+}
+
+bool init()
+{
+    //Init flag
+    bool success = true;
+    //Init SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize. SDL Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+        //Set texture filtering to linear
+        if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+        {
+            printf("Warning: Linear texture filtering not enabled.\n");
+        }
+
+        //Create window
+        gWindow = SDL_CreateWindow("Epic Rock Paper Scissors", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL)
+        {
+            printf("Wdinwo could not be created. SDL Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+            if (gRenderer == NULL)
+            {
+                printf("Renderer could not be created. SDL Error: %s\n", SDL_GetError());
+                success = false;
+            }
+            else
+            {
+                //Init renderer color
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+                //Init PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags))
+                {
+                    printf("SDL_image could not initialize. SDL_image Error: %s\n", IMG_GetError());
+                    success = false;
+                }
+            }
+        }
+    }
+
+    return success;
 }
 
 int main(int argc, char* args[])
